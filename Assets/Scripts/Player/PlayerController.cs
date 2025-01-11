0c13072c2a0470be1203c6658606b3299ae5306d
THE-DANIEL-B45 <danielborgeslazzarini0@gmail.com>
2025-01-10 23:04:34 -0300

    animacoes setadas pela velocidade

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -35,6 +35,7 @@ public class PlayerController : Singleton<PlayerController>
     private Vector3 _pos;
     private float _currentSpeed;
     private Vector3 _startPosition;
+    private float _baseSpeedToAnimation = 7f;
 
     private void Start()
     {
@@ -88,7 +89,7 @@ public class PlayerController : Singleton<PlayerController>
     public void StartToRun()
     {
         _canRun = true;
-        animatorManager.Play(AnimatorManager.AnimationType.RUN);
+        animatorManager.Play(AnimatorManager.AnimationType.RUN,_currentSpeed / _baseSpeedToAnimation);
     }
 
     #region PowerUps
